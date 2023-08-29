9fb85741812ae246f9129ae0394403d0cdcf6010
Eduardo de Campos da Silva <educampos4121@gmail.com>
2023-08-29 15:24:10 -0300

    Guilherme

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var subtrair = require("./modulos/subtrair");
+var multiplicar = require("./modulos/multiplicar");
+var dividir = require("./modulos/dividir");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(subtrair(20,90));
+console.log(multiplicar(20,90));
+console.log(dividir(20,90));
