2ea1453ba650b863d56214b08052de09f1234e52
Guilherme <guiritter7@gmail.com>
2023-09-01 08:55:46 -0300

    GuilhermeR7

--- calculadora.js
+++ calculadora.js
@@ -3,9 +3,12 @@ var media = require("./modulos/media");
 var subtrair = require("./modulos/subtrair");
 var multiplicar = require("./modulos/multiplicar");
 var dividir = require("./modulos/dividir");
+var mensagem = require("./modulos/mensagem");
+var nome = "Guilherme";
 
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(subtrair(20,90));
-console.log(multiplicar(20,90));
-console.log(dividir(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A média de x e y é: " + media(20,90));
+console.log("A subtração de x e y é: " + subtrair(20,90));
+console.log("A multiplicação de x e y é: " + multiplicar(20,90));
+console.log("A divisão de x e y é: " + dividir(20,90));
+console.log(mensagem(nome));
